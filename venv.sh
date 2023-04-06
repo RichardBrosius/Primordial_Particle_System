6c422677bf60a600211e3664855c657dbe52575f
Richard Brosius <chich36@gmail.com>
2023-04-06 19:26:54 -0400

    Fix Notebook

--- venv.sh
+++ venv.sh
@@ -4,11 +4,19 @@ virtualenv venv --python=/usr/bin/python3
 
 if source venv/bin/activate
 then echo activated!
+
+     #Program Libraries
      pip install PyOpenGL PyOpenGL_accelerate
      pip install pygame
      pip install numpy
      pip install cython
      pip install --upgrade cython
      pip install ipython
+
+     #Jupyter notebook modules
+     pip install matplotlib
+     pip install pandas
+     pip install statsmodels
+
 else echo Venv Activation Failed
 fi
